42b3e6c43c3b7bc96a670ab12c9d68428e5b3df3
TK-MUKWEVHO <matodzikutama@gmail.com>
2022-12-18 19:08:14 +0200

    first commit

---
+++ src/test/java/com/example/demo/AzureTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
